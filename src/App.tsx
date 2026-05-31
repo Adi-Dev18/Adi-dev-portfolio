c1d0c9d3165666af4f222d73ee79ed723f059737
Adi-Dev18 <257r5a6715@cmrtc.ac.in>
2026-05-31 14:20:38 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { useLayoutEffect } from "react";
+
 import { Home } from "@/pages/Home";
 
 export default function App() {
+  useLayoutEffect(() => {
+    document.title = "Aditya";
+  }, []);
+
   return <Home />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+document.title = "Aditya";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
